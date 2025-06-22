066c8ac07433b97b5c62c24b47e17520e85c1e80
Austinspeed <austinikedinma@gmail.com>
2025-06-22 15:04:38 +0100

    changed file path for moviecontext

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { MovieProvider } from './context/MovieContext.jsx'
+import { MovieProvider } from './Context/MovieContext.jsx'
 import { ThemeProvider } from './Context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
